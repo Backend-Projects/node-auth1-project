d616959afa912a45a595b5857ed61b165cad3e6c
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-07-28 14:49:44 -0400

    created file structure, installed dependencies, and started server

---
+++ api/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex');
+
+const server = express();
+
+module.exports = server;
