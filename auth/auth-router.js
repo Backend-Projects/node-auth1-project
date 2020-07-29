20685c4902b4d46bee3954b1d63d8f02fdbb7114
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-07-29 16:59:50 -0400

    created login endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -22,7 +22,24 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  //
+  const { username, password } = req.body;
+
+  Users.findBy({ username })
+    .then((users) => {
+      const user = users[0];
+
+      if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.loggedIn = true;
+        req.session.username = user.username;
+
+        res.status(200).json({ message: 'You have logged in!' });
+      } else {
+        res.status(401).json({ message: 'Invalid entry' });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
 });
 
 router.get('/logout', (req, res) => {
