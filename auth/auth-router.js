958469162c2287af40bfecf48ef1106b6480a8bf
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-07-29 14:40:52 -0400

    initial set up of users and auth routers

---
+++ auth/auth-router.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+const Users = require('../users/users-model.js');
+
+router.post('/register', (req, res) => {
+  //
+});
+
+router.post('/login', (req, res) => {
+  //
+});
+
+router.get('/logout', (req, res) => {
+  //
+});
+
+module.exports = router;
