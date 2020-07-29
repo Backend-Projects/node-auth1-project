82df39957ae4911d167cb5c194338de89452565d
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-07-29 16:49:33 -0400

    created register endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -4,7 +4,21 @@ const bcrypt = require('bcryptjs');
 const Users = require('../users/users-model.js');
 
 router.post('/register', (req, res) => {
-  //
+  let creds = req.body;
+
+  const rounds = process.env.HASH_ROUNDS || 4;
+
+  const hash = bcrypt.hashSync(creds.password, rounds);
+
+  creds.password = hash;
+
+  Users.add(creds)
+    .then((saved) => {
+      res.status(201).json({ data: saved });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
 });
 
 router.post('/login', (req, res) => {
